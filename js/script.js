270941253c7b85e18b686c61f0879cbfb57016bd
Annakrictal14 <pro100anyshka@gmail.comgit>
2024-12-11 19:50:43 +0100

    third commit Anya feature

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 document.querySelector('.logo__img').addEventListener('click', () => {
     window.location.href='index.html'
 })
-console.log('asd')
+
+
+const menuBtn = document.querySelector('.burger__menu_btn');
+const menu = document.querySelector('.burger__menu');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');  
+})
+const btn = document.querySelectorAll('.btn')
+const modal = document.querySelector('.show__modal_menu');
+btn.forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault()
+        modal.classList.toggle('active');
+    
+    })
+
+});
